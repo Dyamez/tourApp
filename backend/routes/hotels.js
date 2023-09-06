c454d12648c623d8718ab966eb43c71423c3f759
Dyamez <dyamez@outlook.com>
2023-09-06 11:32:09 -0700

    dirty

---
+++ backend/routes/hotels.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { 
+  createHotel,
+  deleteHotel, 
+  getHotel, 
+  getHotels, 
+  updateHotel } 
+  from '../controllers/HotelController.js';
+import { verifyAdmin } from '../utilites/verifyToken.js'
+
+const router = express.Router();
+
+// CREATE
+router.post ('/', verifyAdmin, createHotel)
+
+// UPDATE
+router.put('/:id', verifyAdmin, updateHotel)
+
+// DELETE
+router.delete('/:id', verifyAdmin, deleteHotel)
+
+// GET
+router.get('/:id', getHotel)
+
+// GET ALL
+router.get('/', getHotels)
+
+export default router;
