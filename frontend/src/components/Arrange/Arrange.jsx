0c6f1c8cf22611d82abe74ff3f65e5d5e46e3991
Dyamez <dyamez@outlook.com>
2023-09-06 09:13:21 -0700

    Update Arrange.jsx

--- frontend/src/components/Arrange/Arrange.jsx
+++ frontend/src/components/Arrange/Arrange.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./../Header/Header";
-import Footer from "./../Footer/footer";
+import Footer from "./../Footer/Footer";
 import Router from "../../router/Router";
 
 export default function Arrange() {
