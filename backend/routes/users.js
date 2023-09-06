410f235c885774e148b28b5ed73ec5383296b159
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-09-06 00:51:14 -0700

    finished roomSchema

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,28 +9,28 @@ import { verifyToken, verifyUser, verifyAdmin } from '../utilites/verifyToken.js
 
 const router = express.Router();
 
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-    res.send('Hello User! You are now logged in!')
-})
+// router.get('/checkauthentication', verifyToken, (req, res, next) => {
+//     res.send('Hello User! You are now logged in!')
+// })
 
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-    res.send('Hello user, You are now logged in and you can delete your account!')
-})
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//     res.send('Hello user, You are now logged in and you can delete your account!')
+// })
 
-router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
-    res.send('Hello admin, you are now logged in and you can delete all accounts!')
-})
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//     res.send('Hello admin, you are now logged in and you can delete all accounts!')
+// })
 
 // UPDATE
-router.put('/:id', updateUser)
+router.put('/:id', verifyUser, updateUser)
 
 // DELETE
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyUser, deleteUser)
 
 // GET
-router.get('/:id', getUser)
+router.get('/:id', verifyUser, getUser)
 
 // GET ALL
-router.get('/', getUsers)
+router.get('/', verifyAdmin, getUsers)
 
 export default router;
