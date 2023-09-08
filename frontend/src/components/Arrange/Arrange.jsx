f1a1dcafec30d5cc1853e23e052b3e7276cf0b1f
Dyamez <dyamez@outlook.com>
2023-09-07 22:24:21 -0700

    tweak again

--- frontend/src/components/Arrange/Arrange.jsx
+++ frontend/src/components/Arrange/Arrange.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./../Header/Header";
-import Footer from "./../Footer/Footer";
+//import Footer from "./../Footer/Footer"; <Footer />
 import Router from "../../router/Router";
 
 export default function Arrange() {
@@ -9,7 +9,6 @@ export default function Arrange() {
     <>
       <Header />
       <Router />
-      <Footer />
     </>
   );
 }
