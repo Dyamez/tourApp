a3824d1b74732d7a282edfd6bb24892a0b2b47a3
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-09-10 12:30:29 -0500

    tweaks

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,13 +1,9 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
 import { createError } from "../utilities/errored.js";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
-    //const salt = bcrypt.genSaltSync(10);
-    //const hash = bcrypt.hashSync(req.body.password, salt);
-
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
